482da15f531541d1329d169129a8b6a061a7e64b
Halililyas <halililyaskap@gmail.com>
2023-11-06 13:43:17 +0300

    FACEDE_DESİNG
    Kullanımı

---
+++ Facede_Desing/Facede_Desing/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facede_Desing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.Save();
+            Console.ReadLine();
+        }
+    interface  ILogging
+        {
+            void Log();
+        }
+        interface ICaching
+        {
+            void Caching();
+        }
+        interface IAuthorize
+        {
+            void UserCheck();
+        }
+
+        class Logging : ILogging
+        {
+            public void Log()
+            {
+                Console.WriteLine("Logged");
+            }
+        }
+        class Caching : ICaching
+        {
+            void ICaching.Caching()
+            {
+                Console.WriteLine("Caching");
+            }
+        }
+        class Authirize : IAuthorize
+        {
+            public void UserCheck()
+            {
+                Console.WriteLine("User Check");
+            }
+        }
+
+        class Customer
+        {
+            private CrossCuttongConcreadFacade _concreadFacade;
+            public Customer()
+            {
+                _concreadFacade = new CrossCuttongConcreadFacade();
+            }
+            public void Save()
+            {
+                _concreadFacade.Caching.Caching();
+                _concreadFacade.Logging.Log();
+                _concreadFacade.authorize.UserCheck();
+                Console.WriteLine("Save");
+            }
+
+        }
+        class CrossCuttongConcreadFacade
+        {
+            public ILogging Logging;
+            public ICaching Caching;
+            public IAuthorize authorize;
+
+            public CrossCuttongConcreadFacade()
+            {
+                Logging = new Logging();
+                Caching = new Caching();
+                authorize = new Authirize();
+            }
+        }
+    }
+}
